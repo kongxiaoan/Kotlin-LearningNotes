190d80be1bbfc6110cca32a465cfa4114d3b5fd0
kongxiaoan <18153976553@163.com>
2017-12-15 14:35:45 +0800

    1、更新说明 添加测试类 完整初始化project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Kotlin-LearningNotes
 Kotlin 笔记
+### 一、base 包
+#### 1、kotlin的基础语法
+
---
+++ app/src/main/java/cn/tcm/kotlin_learningnotes/base/Test.kt
@@ -0,0 +1,13 @@
+package cn.tcm.kotlin_learningnotes.base
+
+/**
+ * Created by mr.kong on 2017/12/15.
+ * 测试类
+ */
+
+object Test {
+	@JvmStatic
+	fun main(args: Array<String>) {
+
+	}
+}
